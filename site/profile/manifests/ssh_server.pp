735aecf0c8a675faa58e9619d47d674732a8ba93
akeruzunosmanoglu <75013943+akeruzunosmanoglu@users.noreply.github.com>
2020-11-27 22:03:59 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -1,4 +1,4 @@
-class profile::ssh_server (
+class profile::ssh_server {
   package { 'openssh-server':
     ensure => present,
   }
@@ -10,7 +10,7 @@ class profile::ssh_server (
     ensure => present,
     user => 'root',
     type => 'ssh-rsa',
-    key  =>'AAAAB3NzaC1yc2EAAAADAQABAAABAQDyYFLvxpJdqMSdCrP9WyyO1ThfxzOylvcvGiA5vsCBrThscT6MszLq1xlKwMdfaf1hE/aY/2+OHV2JwRBntHPGxtRnIQMzJnovID20Z5591+LIggdDPrtHHKru0y4QBx685I77x+asB5sKia0LsjI+zhtP3jnrWxkwQxVFCT1t+9gSCsKIJaA+j+eYKBokmTn8oil8QLUY3TVQaVfrTnUHZQiW4IHsZZ+GJfZczXaWuQ3sRoK7xM8/+DnuZYXwcaL3NIQMxoJTao2yyNmn+FNV9vs5muL+KQciCe//KXmqOoPz6X1Nc7vDsBTaQFrAJS29b9bMadY/EEE8+C7HpLXN',
+    key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDyYFLvxpJdqMSdCrP9WyyO1ThfxzOylvcvGiA5vsCBrThscT6MszLq1xlKwMdfaf1hE/aY/2+OHV2JwRBntHPGxtRnIQMzJnovID20Z5591+LIggdDPrtHHKru0y4QBx685I77x+asB5sKia0LsjI+zhtP3jnrWxkwQxVFCT1t+9gSCsKIJaA+j+eYKBokmTn8oil8QLUY3TVQaVfrTnUHZQiW4IHsZZ+GJfZczXaWuQ3sRoK7xM8/+DnuZYXwcaL3NIQMxoJTao2yyNmn+FNV9vs5muL+KQciCe//KXmqOoPz6X1Nc7vDsBTaQFrAJS29b9bMadY/EEE8+C7HpLXN',
   
   }
-)
+}
