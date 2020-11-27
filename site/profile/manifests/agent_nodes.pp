8c8af96cdfbb8fc72749e07047fed13beb4ff9f3
akeruzunosmanoglu <75013943+akeruzunosmanoglu@users.noreply.github.com>
2020-11-27 21:59:16 +0100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,5 +2,14 @@ class profile::agent_nodes {
    include dockeragent
    dockeragent::node {'web.network.lan':}
    dockeragent::node {'db.network.lan':}
+   
+   host {'web.network.lan':
+     ensure => present,
+     ip => '172.18.0.2',
+    }
+    host {'db.network.lan':
+     ensure => present,
+     ip => '172.18.0.3',
+    }
 
 }
