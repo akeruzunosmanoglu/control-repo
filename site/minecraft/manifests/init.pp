7d57291be2cc421e959ffcc7fc170c567825de51
akeruzunosmanoglu <75013943+akeruzunosmanoglu@users.noreply.github.com>
2020-11-28 09:21:37 +0100

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,30 @@
+class minecraft (
+  $url = 'https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar',
+  $install_dir = '/opt/minecraft'
+){
+  file { $install_dir:
+    ensure => directory,
+  }
+  file { "${install_dir}/minecraft_server.jar":
+    ensure => file,
+    source => $url,
+  }
+
+  package {'java':
+    ensure => present,
+  }
+  file {"${install_dir}/eula.txt":
+    ensure => file,
+    content => 'eula=true'
+  }
+  file {‘/etc/systemd/system/minecraft.service’:
+    ensure => file,
+    content => epp(‘minecraft/minecraft.service.epp’, {
+      install_dir => $install_dir,
+    }),
+  }
+  service { 'minecraft':
+    ensure => running,
+    enable => true,
+  }
+}
